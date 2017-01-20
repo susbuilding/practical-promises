ca046b020f61fedc0f08f4a148e1f5ed7abb36d7
Laura Chung <3llejeong@gmail.com>
2017-01-20 14:43:32 -0500

    completed D

--- exercise-one.js
+++ exercise-one.js
@@ -33,13 +33,21 @@ function problemA () {
    */
 
   // callback version
-  readFile('poem-one/stanza-01.txt', function (err, stanza) {
-    console.log('-- A. callback version --');
-    blue(stanza);
-  });
+  // readFile('poem-one/stanza-01.txt', function (err, stanza) {
+  //   console.log('-- A. callback version --');
+  //   blue(stanza);
+  // });
 
   // promise version
-  // ???
+  var path = 'poem-one/stanza-01.txt';
+
+  promisifiedReadFile(path).then(function(success){
+    blue(success.toString());
+  }).catch(function(err){
+    console.error(err);
+  })
+
+
 
 }
 
@@ -52,17 +60,31 @@ function problemB () {
    */
 
   // callback version
-  readFile('poem-one/stanza-02.txt', function (err, stanza2) {
-    console.log('-- B. callback version (stanza two) --');
-    blue(stanza2);
-  });
-  readFile('poem-one/stanza-03.txt', function (err, stanza3) {
-    console.log('-- B. callback version (stanza three) --');
-    blue(stanza3);
-  });
+  // readFile('poem-one/stanza-02.txt', function (err, stanza2) {
+  //   console.log('-- B. callback version (stanza two) --');
+  //   blue(stanza2);
+  // });
+  // readFile('poem-one/stanza-03.txt', function (err, stanza3) {
+  //   console.log('-- B. callback version (stanza three) --');
+  //   blue(stanza3);
+  // });
 
   // promise version
   // ???
+  var path1 = 'poem-one/stanza-02.txt';
+  var path2 = 'poem-one/stanza-03.txt';
+
+  promisifiedReadFile(path1).then(function(success){
+    blue(success.toString());
+  }).catch(function(err){
+    console.error(err);
+  })
+
+    promisifiedReadFile(path2).then(function(success){
+    blue(success.toString());
+  }).catch(function(err){
+    console.error(err);
+  })
 
 }
 
@@ -76,18 +98,43 @@ function problemC () {
    */
 
   // callback version
-  readFile('poem-one/stanza-02.txt', function (err, stanza2) {
-    console.log('-- C. callback version (stanza two) --');
-    blue(stanza2);
-    readFile('poem-one/stanza-03.txt', function (err, stanza3) {
-      console.log('-- C. callback version (stanza three) --');
-      blue(stanza3);
-      console.log('-- C. callback version done --');
-    });
-  });
+  // readFile('poem-one/stanza-02.txt', function (err, stanza2) {
+  //   console.log('-- C. callback version (stanza two) --');
+  //   blue(stanza2);
+  //   readFile('poem-one/stanza-03.txt', function (err, stanza3) {
+  //     console.log('-- C. callback version (stanza three) --');
+  //     blue(stanza3);
+  //     console.log('-- C. callback version done --');
+  //   });
+  // });
 
   // promise version (hint: don't need to nest `then` calls)
-  // ???
+  var path1 = 'poem-one/stanza-02.txt';
+  var path2 = 'poem-one/stanza-03.txt';
+
+  var prom1 = promisifiedReadFile(path1)
+  // .then(function(success){
+  //   // blue(success.toString());
+  // }).catch(function(err){
+  //   console.error(err);
+  // })
+
+    var prom2 = promisifiedReadFile(path2)
+  //   .then(function(success){
+  //   // blue(success.toString());
+  // }).catch(function(err){
+  //   console.error(err);
+  // })
+
+
+  Promise.all([prom1, prom2])
+  .then(function(success){
+    success.forEach(function(ele,idx){
+      blue(success[idx]);
+    })
+  }).catch(function(error){
+    console.error(error);
+  })
 
 }
 
@@ -99,14 +146,20 @@ function problemD () {
    */
 
   // callback version
-  readFile('poem-one/wrong-file-name.txt', function (err, stanza4) {
-    console.log('-- D. callback version (stanza four) --');
-    if (err) magenta(err);
-    else blue(stanza4);
-  });
+  // readFile('poem-one/wrong-file-name.txt', function (err, stanza4) {
+  //   console.log('-- D. callback version (stanza four) --');
+  //   if (err) magenta(err);
+  //   else blue(stanza4);
+  // });
 
   // promise version
-  // ???
+  var path = 'poem-one/wrong-file-name.txt';
+
+  promisifiedReadFile(path).then(function(resolve){
+    blue(resolve);
+  }).catch(function(err){
+    magenta(err);
+  });
 
 }
 
